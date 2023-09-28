8c6c290b89b4f0c7897be56c35cf3538cdd46a68
Margaret Irungu <mwirungu22@gmail.com>
2023-09-28 14:18:36 +0100

    Additional notes and some spelling error rectification

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,22 +5,22 @@ const userRouter = Router();
 
 //import controllers to give routes functionality
 const {registerUser, listAllUsers, deleteUser, updatePassword, loginUser } = require("../controllers/controllers");
-//automatically importted when you insert hashPassword function
+//automatically imported when you insert hashPassword function
 const {hashPassword, passwordCheck, tokenCheck} = require("../middleware");
 
 //create a user
-userRouter.post("/users/register",hashPassword, registerUser);
+userRouter.post("/users/register",hashPassword, registerUser);// password protected by hashing using bcrypt
 
 //list all users in db
-userRouter.get("/users/listAllUsers", tokenCheck, listAllUsers);
+userRouter.get("/users/listAllUsers", tokenCheck, listAllUsers);//secured with token check (jwt)
 
 //delete a user from db
-userRouter.delete("/users/deleteUser", passwordCheck, deleteUser);
+userRouter.delete("/users/deleteUser", passwordCheck, deleteUser); //password check checks if the password entered and hashed password are the same
 
 //updating user password
-userRouter.put("/users/updatePassword", passwordCheck, hashPassword, updatePassword);
+userRouter.put("/users/updatePassword", passwordCheck, hashPassword, updatePassword);// password protected by hashing using bcrypt
 
 //login in user and issue a new web token upon login in
-userRouter.get("/users/login", passwordCheck, loginUser);
+userRouter.get("/users/login", passwordCheck, loginUser); //password check to make sure they match to details in database token check not needed as user will have one generated upon log in
 //export the routes
 module.exports = userRouter;
