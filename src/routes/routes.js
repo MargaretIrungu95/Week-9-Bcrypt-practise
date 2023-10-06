bda657d6cdf814a0e367d23e1b33eb97d5b4a134
Margaret Irungu <mwirungu22@gmail.com>
2023-10-06 16:38:46 +0100

    list all users changed

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,7 +12,7 @@ const {hashPassword, passwordCheck, tokenCheck} = require("../middleware");
 userRouter.post("/users/register",hashPassword, registerUser);// password protected by hashing using bcrypt
 
 //list all users in db
-userRouter.get("/users/listAllUsers", tokenCheck, listAllUsers);//secured with token check (jwt)
+userRouter.get("/listAllUsers", tokenCheck, listAllUsers);//secured with token check (jwt)
 
 //delete a user from db
 userRouter.delete("/users/deleteUser", passwordCheck, deleteUser); //password check checks if the password entered and hashed password are the same
